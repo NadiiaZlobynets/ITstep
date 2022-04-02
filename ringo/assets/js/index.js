f5bd32f8f018580b3ac94958dc1490b1fc384833
Nadiia.Zlobynets <euphoria93@gmail.com>
2022-04-02 20:43:40 +0100

    #2 Lesson: added adaptation for mobile devices

--- ringo/assets/js/index.js
+++ ringo/assets/js/index.js
@@ -2,6 +2,11 @@ import menu from "./component/menu.js";
 import garbage from './component/garbage.js';
 import slider from "./component/slider.js";
 
+function menuOnClick () {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     menu();
     garbage();
